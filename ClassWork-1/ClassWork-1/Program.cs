9bb9f10c447e637d16e123be605101c6f44d2de0
Hassan AL-Haddad <76395224+Hassan-ALHaddad@users.noreply.github.com>
2020-12-20 19:28:53 +0300

    CW1

---
+++ ClassWork-1/ClassWork-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassWork_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "Hero";
+            float speed = 25.5f;
+            int age = 22;
+            string superPower1 = "steel";
+            string superPower2 = "night vision";
+
+
+            Console.WriteLine("My name is " + characterName + " my power is " + superPower1 +
+                " armor and " + superPower2 + ". my age is " + age + " ,and speed is " + speed);
+
+        }
+    }
+}
