5c9225eaccd169240cfb5576a5c707a112e555d0
Hassan AL-Haddad <76395224+Hassan-ALHaddad@users.noreply.github.com>
2020-12-21 17:52:06 +0300

    ClassWork Exrcise2

--- ClassWork-1/ClassWork-1/Program.cs
+++ ClassWork-1/ClassWork-1/Program.cs
@@ -6,15 +6,28 @@ namespace ClassWork_1
     {
         static void Main(string[] args)
         {
+            //ex1
             string characterName = "Hero";
             float speed = 25.5f;
             int age = 22;
-            string superPower1 = "steel";
+            string superPower1 = "steel armor";
             string superPower2 = "night vision";
 
 
             Console.WriteLine("My name is " + characterName + " my power is " + superPower1 +
-                " armor and " + superPower2 + ". my age is " + age + " ,and speed is " + speed);
+                " and " + superPower2 + ". my age is " + age + " ,and speed is " + speed);
+
+            //ex2
+            string heroName = "Hassan";
+            int heroHeight = 170;
+            int heroAge = 20;
+            string heroSuperPower1 = "gold armor";
+            string heroSuperPower2 = "invisible";
+
+            int ageDifference = age - heroAge;
+            Console.WriteLine("The different age between " + characterName + " and " + heroName + " is " + ageDifference);
+
+
 
         }
     }
