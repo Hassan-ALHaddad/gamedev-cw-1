83f14222194745e167a659758aa12aad1d52b534
Hassan AL-Haddad <76395224+Hassan-ALHaddad@users.noreply.github.com>
2020-12-21 18:47:29 +0300

    CW-1-2

---
+++ CW-1-2/CW-1-2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CW_1_2
+{
+    class Program
+    {
+        static double playerSpeed;
+        static void Main(string[] args)
+        {
+            static void SetSpeed()
+            {
+                playerSpeed = -2.5;
+            }
+
+            static void GetSpeed()
+            {
+                Console.WriteLine("The current player speed is " + playerSpeed);
+            }
+
+            SetSpeed();
+            GetSpeed();
+
+            
+
+           
+        }
+    }
+}
